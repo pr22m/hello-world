f439ad9ce35c436aa312b96055fc4f39e243dde4
ssitl <yyy666@yandex.ru>
2016-11-03 22:26:09 +0300

    http server
    http server text

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-function test()
-{
-  console.log("test");
-}
+var http = require("http");
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen();
